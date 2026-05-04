254eede67ca76ee9735e9c3ffe579b6b056bd544
mca1774 <mcA1774_mcdcorp@external.mcd.com>
2026-05-04 08:08:04 +0530

    Initial Commit

---
+++ testXcBuild/testXcBuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  testXcBuildApp.swift
+//  testXcBuild
+//
+//  Created by Ankit Patel on 04/05/26.
+//
+
+import SwiftUI
+
+@main
+struct testXcBuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testXcBuildTests/testXcBuildTests.swift
@@ -0,0 +1,17 @@
+//
+//  testXcBuildTests.swift
+//  testXcBuildTests
+//
+//  Created by Ankit Patel on 04/05/26.
+//
+
+import Testing
+@testable import testXcBuild
+
+struct testXcBuildTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
